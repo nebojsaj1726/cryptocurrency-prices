86d6bcc74bacdb234e7656cc5620b2cb459e3115
nebojsaj1726 <nebojsa.jacovic@gmail.com>
2020-09-09 10:14:07 +0200

    Updates

---
+++ precache-manifest.c4c360e0eac40b5fc339cb004f842fe1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90d69800ec6a5df374818e734026e180",
+    "url": "/nebojsaj1726/cryptocurrency-prices/index.html"
+  },
+  {
+    "revision": "42a4302cdc7f15c0a0b0",
+    "url": "/nebojsaj1726/cryptocurrency-prices/static/css/2.ba242066.chunk.css"
+  },
+  {
+    "revision": "eec3aded861a0d524c27",
+    "url": "/nebojsaj1726/cryptocurrency-prices/static/css/main.31591eb5.chunk.css"
+  },
+  {
+    "revision": "42a4302cdc7f15c0a0b0",
+    "url": "/nebojsaj1726/cryptocurrency-prices/static/js/2.27a1a4b1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nebojsaj1726/cryptocurrency-prices/static/js/2.27a1a4b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eec3aded861a0d524c27",
+    "url": "/nebojsaj1726/cryptocurrency-prices/static/js/main.332f4ad7.chunk.js"
+  },
+  {
+    "revision": "7f46a44577bf490b4483",
+    "url": "/nebojsaj1726/cryptocurrency-prices/static/js/runtime-main.abf55cfd.js"
+  }
+]);
